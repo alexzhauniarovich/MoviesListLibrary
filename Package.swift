d763ba80c3f9faf5dea6803c5b1c08803cde686e
Alex Zhauniarovich <aliaksei.zhauniarovich@jaydevs.com>
2023-09-27 15:12:01 +0200

    Implemented trending movies list

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["MoviesListLibrary"]),
     ],
     dependencies: [
-        .package(url: "../Presentation", from: "1.0.0")
+//        .package(url: "../Presentation", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
